390826d7989c9a4bd350f55657f64f8e10827a91
KIMINHEON19-501993 <97835021+KIMINHEON19-501993@users.noreply.github.com>
2022-03-10 11:37:13 +0900

    Delete animal directory

--- animal/Cat.java
+++
@@ -1,9 +0,0 @@
-package animal;
-
-public class Cat extends Animal{
-
-	@Override
-	public void speak() {
-		System.out.println("¾ß¿Ë~");
-	}
-}
--- animal/Fish.java
+++
@@ -1,9 +0,0 @@
-package animal;
-
-public class Fish extends Animal{
-	@Override
-	public void walk() {
-		System.out.println("헤엄쳐 다녀요.");
-	}
-	
-}
